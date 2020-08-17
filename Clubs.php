0138a8d8d849866f7cfd28b47b1fe63bd52c7d56
Mario Maricevic <mmaricevic123@gmail.com>
2020-08-17 16:12:24 +0200

    Napravljena Naslovna.php i Clubs.php. Potrebno dodati adminske ovlasti.

--- Clubs.php
+++ Clubs.php
@@ -1,10 +1,112 @@
 <?php
 session_start();
 include("Navbar.php");
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+  include("DBI.php");  //informacije o bazi podataka
+
+  if(isset($_POST["clubleave"])){   //Izlaz iz kluba
+    $e_mail = $_SESSION['e_mail'];
+    $izadi_query = $mysqli->query("UPDATE korisnici SET ID_kluba = NULL WHERE Email = '$e_mail'");
+    //header("location: Clubs.php");
+  }
+
+  if(isset($_POST["clubidjoin"])){   //Pridruzivanje klubu
+    $klub = $_POST['clubidjoin'];
+    $e_mail = $_SESSION['e_mail'];
+    $pridruzi_se_query = $mysqli->query("UPDATE korisnici SET ID_kluba = $klub WHERE Email = '$e_mail'");
+    //header("location: Clubs.php");
+  }
+}
 ?>
 
 <body id= "ron_klubovi">
+  <div class="container" style="padding-top: 60px;padding-bottom: 40px;">
+    <div class="row text-center">
+      <div class="col-md-1"></div>
+      <div class="col-md-10">
+        <?php
+          include("DBI.php");  //informacije o bazi podataka
+          echo "<table class='table table-dark'>";
+          if(!isset($_GET['clubid'])){
+            echo "<thead>
+                    <tr class='table-info'>
+                    <th scope='col'></th>
+                    <th scope='col'>Ime kluba</th>
+                    </tr>
+                 </thead>";
+           echo "<tbody>";
+          $result = $mysqli->query("SELECT * FROM ron_klubovi");
+          while($row = $result -> fetch_assoc()){
+              echo "<tr>
+                <td>
+                  <a href= 'Clubs.php?clubid=" . $row["ID_kluba"] . "'>
+                    <img src='Club_images/" . $row["Slika"] . "' class = 'profile-pic' alt='Slika profila'>
+                    </a>
+                </td>";
+              echo "<td style='vertical-align: middle;'>
+                      <a style='color:white;' href= 'Clubs.php?clubid=" . $row["ID_kluba"] . "'>
+                      " . $row["Ime"] . "
+                      </a>
+                    </td>";
+              echo "</tr>";
+            }
+          echo "</tbody>";
+          }
+          else{
+            $klub = $_GET['clubid']; //dohvacanje ID kluba
+            $ime_kluba_query = $mysqli->query("SELECT Ime FROM ron_klubovi WHERE ID_kluba = $klub"); //dohvacanje imena kluba na temelju ID
+            $ime_kluba = $ime_kluba_query->fetch_assoc();
 
+            if(isset($_SESSION['e_mail'])){ //ako je netko logiran, dohvati njegov klub
+                $e_mail = $_SESSION['e_mail'];
+                $osoba_query = $mysqli->query("SELECT ID_kluba FROM korisnici WHERE Email = '$e_mail'"); //dohvati ID_kluba trenutno logirane osobe
+                $klub_osobe = $osoba_query->fetch_assoc();
+            }
+
+            $clanovi_query =  $mysqli->query("SELECT Ime,Prezime,Slika FROM korisnici WHERE ID_kluba = $klub"); //dohvati clanove
+
+            //Zaglavlje tablice clanova
+            echo "<thead>
+                    <tr class='table-info'>
+                    <th scope='col'>" . $ime_kluba["Ime"];
+            if(isset($_SESSION['e_mail'])){            //$klub_osobe['ID_kluba'] == NULL
+                if($klub_osobe['ID_kluba'] == $klub){
+                  echo "<form action = 'Clubs.php' method= 'POST'>
+                        <button type='submit' class='btn btnleaveclub' name='clubleave'>Napusti klub</button>
+                        </form>";
+                }
+                else if($klub_osobe['ID_kluba'] == NULL){
+                  echo "<form action = 'Clubs.php' method= 'POST'>
+                      <button type='submit' class='btn btnjoinclub' name='clubidjoin' value='".$klub."'>Pridruzi se</button>
+                      </form>";
+                }
+            }
+            echo "</th>
+                  <th style='vertical-align: middle;' scope='col' colspan='2'>Popis clanova</th>
+                  </tr>
+                 </thead>";
+
+            //Ispis clanova kluba
+             echo "<tbody>";
+            while($row = $clanovi_query -> fetch_assoc()){
+              echo "<tr>";
+              echo"<td>
+                    <img src='profile_images/" . $row["Slika"] . "' class = 'profile-pic' alt='Slika profila'>
+                  </td>";
+              echo "<td style='vertical-align: middle;'>" . $row["Ime"] . "</td>";
+              echo "<td style='vertical-align: middle;'>" . $row["Prezime"] . "</td>";
+              echo "</tr>";
+            }
+            echo "</tbody>";
+          }
+          echo "</table>";
+        ?>
+      </div>
+      <div class="col-md-1"></div>
+    </div>
+  </div>
 
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
--- Homepage.php
+++ Homepage.php
@@ -1,11 +1,112 @@
 <?php
-session_start();
-include("Navbar.php");
+ session_start();
+ include("Navbar.php");
 ?>
 
 <body id= "naslovna">
 
+  <section id = "uvod">
+  <div id="slideshow" class="carousel slide carousel-fade" data-ride="carousel">
+      <!-- Indicator dots -->
 
+      <ol class="carousel-indicators">
+          <li data-target="#slideshow" data-slide-to="0" class="active"></li>
+          <li data-target="#slideshow" data-slide-to="1"></li>
+          <li data-target="#slideshow" data-slide-to="2"></li>
+      </ol>
+
+      <!-- Wraper for slides -->
+
+        <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="AllPictures/Slideshow1.jpg"/>
+                <div class="carousel-caption first-cap">
+                    <h1>Ronjenje je vaša strast?</h1>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="AllPictures/Slideshow2.jpg"/>
+                <div class="carousel-caption sec-cap">
+                  <h1>More je vaš drugi dom?</h1>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="AllPictures/Slideshow3.jpg"/>
+                <div class="carousel-caption third-cap">
+                  <h1>Ljubimac ste morskih dubina?</h1>
+                </div>
+            </div>
+        </div>
+
+      <!-- Controls - next and prev buttons-->
+
+        <a class="carousel-control-prev" href="#slideshow" role="button" data-slide="prev">
+           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+           <span class="sr-only">Previous</span>
+        </a>
+
+        <a class="carousel-control-next" href="#slideshow" role="button" data-slide="next">
+           <span class="carousel-control-next-icon" aria-hidden="true"></span>
+           <span class="sr-only">Next</span>
+        </a>
+
+    </div>
+  </section>
+  <section id = "services">
+    <div class="container">
+			<h1>Što sve pruža ARA sustav?</h1>
+			<div class="row services">
+				<div class="col-md-4 text-center" onclick="location.href='Dive.php';">
+					<div class="circle-icon">
+						<img class = "imgicon" src = "AllPictures/ServicesDiveIcon.png">
+					</div>
+					<h3>Planiranje urona</h3>
+					<p>Odaberite jedan od ponuđenih lokaliteta, kreirajte događaj, pozovite ekipu i avantura može početi!</p>
+				</div>
+				<div class="col-md-4 text-center" onclick="location.href='Register.php';">
+					<div class="circle-icon">
+					<img class = "imgicon" src = "AllPictures/ServicesNewUserIcon.png">
+					</div>
+					<h3>Vlastiti profil</h3>
+					<p>Kreirajte svoj profil kako bi iskoristili sve mogućnosti našeg sustava i bili dio ARA obitelji!</p>
+				</div>
+				<div class="col-md-4 text-center" onclick="location.href='EcoActions.php';">
+					<div class="circle-icon">
+						<img class = "imgicon" src = "AllPictures/ServicesEcoActionIcon.png">
+					</div>
+					<h3>EKO akcije</h3>
+					<p>Očuvanje čistoće mora i okoliša naša je zadaća. Budi i ti dio EKO akcija i pruži pomoć svojoj prirodi! </p>
+				</div>
+				<div class="col-md-4 text-center" onclick="location.href='Clubs.php';">
+					<div class="circle-icon">
+						<img class = "imgicon" src = "AllPictures/ServicesClubIcon.png">
+					</div>
+					<h3>Ronilački timovi</h3>
+					<p>Još uvijek nemaš svoj tim? Pridruži se timu, upoznaj nove ljude i iskusi čari timskog ronjenja.</p>
+				</div>
+				<div class="col-md-4 text-center" onclick="location.href='Contact.php';">
+					<div class="circle-icon">
+							<img class = "imgicon" src = "AllPictures/ServicesContactIcon.png">
+					</div>
+					<h3>Informacije</h3>
+					<p>Svoje ideje, mišljenja, komentare i kritike u vezi našeg sustava slobodno proslijedite. Mi smo tu za Vas!</p>
+				</div>
+				<div class="col-md-4 text-center" onclick="location.href='<?php if(isset($_SESSION['e_mail'])){
+                      echo "Results.php";}
+                  else{
+                      echo "Register.php";
+                  }
+          ?>';">
+					<div class="circle-icon">
+							<img class = "imgicon" src = "AllPictures/ServicesResultIcon.png">
+					</div>
+					<h3>Statistika ronjenja</h3>
+					<p>Kreiranjem profila u mogućnosti ste pratiti sve Vaše dosadašnje događaje i rezultate te pratiti Vaš napredak!</p>
+				</div>
+			</div>
+
+		</div>
+  </section>
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
